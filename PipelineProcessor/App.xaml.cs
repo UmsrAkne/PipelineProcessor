ee0aaaac0859196baf6c5857f0726bdad73cc96c
UmsrAkne <sibata486@gmail.com>
2025-11-30 05:06:10 +0900

    init

---
+++ PipelineProcessor/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using PipelineProcessor.Views;
+using Prism.Ioc;
+
+namespace PipelineProcessor;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<MainWindow>();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+    }
+}
---
+++ PipelineProcessor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PipelineProcessor.Utils;
+using Prism.Mvvm;
+
+namespace PipelineProcessor.ViewModels;
+
+public class MainWindowViewModel : BindableBase
+{
+    private string title = new AppVersionInfo().Title;
+
+    public string Title
+    {
+        get => title;
+        set => SetProperty(ref title, value);
+    }
+}
